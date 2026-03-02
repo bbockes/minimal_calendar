3363a143b37c45a340a7634e6f9abed5cd9c0232
Brendan <59584595+bbockes@users.noreply.github.com>
2026-03-02 12:20:46 -0500

    Checkpoint

--- main.js
+++ main.js
@@ -1,5 +1,35 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
+const fs = require('fs');
+const os = require('os');
+
+ipcMain.handle('print-html', async (_event, html) => {
+  const tmpPath = path.join(os.tmpdir(), `minimal-calendar-print-${Date.now()}.html`);
+  fs.writeFileSync(tmpPath, html, 'utf8');
+
+  const printWin = new BrowserWindow({
+    show: false,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  return new Promise((resolve) => {
+    printWin.webContents.once('did-finish-load', () => {
+      // Brief delay so fonts (e.g. Work Sans) can render before print
+      setTimeout(() => {
+        printWin.webContents.print({ silent: false }, (success, failureReason) => {
+          printWin.destroy();
+          try { fs.unlinkSync(tmpPath); } catch (_) {}
+          resolve({ success, failureReason });
+        });
+      }, 300);
+    });
+
+    printWin.loadFile(tmpPath);
+  });
+});
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -11,6 +41,7 @@ function createWindow() {
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js'),
     },
   });
 
---
+++ preload.js
@@ -0,0 +1,6 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  printHtml: (html) => ipcRenderer.invoke('print-html', html),
+  isElectron: true,
+});
